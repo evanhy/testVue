cf51a7e418ee2940f14e9be0aceac9098c1314cf
huyu <15556151723@163.com>
2022-12-16 15:20:58 +0800

    init test

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
     <h2>Welcome to join and complete this project with resumes.</h2>
     <h3>尝试自己搭架子</h3>
     <h3>没有接口数据，所以学习mock数据</h3>
+    <h3><a href="https://github.com/pan52yu/testVue" target="_blank">Github地址</a>为：https://github.com/pan52yu/testVue</h3>
     <img alt="Vue logo" src="./assets/logo.png">
 </template>
 
