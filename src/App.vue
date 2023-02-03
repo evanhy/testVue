dacd01cc23df555ed9ed1c123fb7609995e444a8
huyu <15556151723@163.com>
2023-02-03 15:17:29 +0800

    refactor: 使用Vue3 + Ts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
- <script lang="ts">
-
+<script setup lang="ts">
+import ToDo from "./components/ToDo.vue";
 </script>
 
 <template>
-    <h1>I am a Vue 3 + TS learning program.</h1>
-    <h2>Welcome to join and complete this project with resumes.</h2>
-    <h3>尝试自己搭架子</h3>
-    <h3>没有接口数据，所以学习mock数据</h3>
-    <h3><a href="https://github.com/pan52yu/testVue" target="_blank">Github地址</a>为：https://github.com/pan52yu/testVue</h3>
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <to-do></to-do>
+  </div>
 </template>
 
-<style>
-#app {
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+defineOptions({
+    name: 'ToDo',
+    // inheritAttrs: false,
+})
+const nowTime = ref(new Date());
+
+//格式化时间
+const dateFormat = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    const minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    let week = date.getDay(); // 星期
+    let weekArr = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六",];
+    return (
+        year + "年" + month + "月" + day + "日 " + hours + ":" + minutes + ":" + seconds + " " + weekArr[week]
+    );
+}
+</script>
+
+<template>
+    <h1>{{ nowTime }}</h1>
+</template>
+
+<style scoped>
+
+</style>
