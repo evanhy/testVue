1c376425383a45aca1a92fbbecde40bac0607e4e
huyu <yhu8836@gmail.com>
2023-06-26 16:41:54 +0800

    feat: modify the router mode to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,18 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
 
 import baseRouters from './modules/base'
 import vueUseRouters from './modules/vueUse'
-import echartsRouters from "@/router/modules/echarts";
+import echartsRouters from "./modules/echarts";
 import proTable from './modules/proTable';
-import playground from "@/router/modules/playground";
-import vueDraggable from "@/router/modules/draggable";
+import playground from "./modules/playground";
+import vueDraggable from "./modules/draggable";
 import components from "./modules/components"
 
 const routes: RouteRecordRaw[] = [...baseRouters, ...vueUseRouters, ...echartsRouters, ...proTable, ...playground, ...vueDraggable, ...components]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    // history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes,
     scrollBehavior() {
         return {
